213316e6d4ab34ac8a1e03b22e3e179f7eda80a3
Tom Kirkpatrick <tom@strike.me>
2024-02-25 22:19:33 +0000

    Add bundkled code

---
+++ dist/495.index.js
@@ -0,0 +1,24 @@
+export const id = 495;
+export const ids = [495];
+export const modules = {
+
+/***/ 70495:
+/***/ ((module) => {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(() => {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = () => ([]);
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = 70495;
+module.exports = webpackEmptyAsyncContext;
+
+/***/ })
+
+};
