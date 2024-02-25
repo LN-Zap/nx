6921b2da268b3c3ab2a3e0c39f0762b8228e5b0d
Tom Kirkpatrick <tom@strike.me>
2024-02-25 23:06:04 +0000

    Install node before using

--- src/setup.ts
+++ src/setup.ts
@@ -11,5 +11,9 @@ const filePath = fileURLToPath(moduleUrl)
 // Get the directory of the current script
 const dir = dirname(filePath)
 
+// Install the required Node.js version
+execSync('npx actions/setup-node@v4.0.2', { cwd: dir, stdio: 'inherit' });
+execSync('node -v', { cwd: dir, stdio: 'inherit' });
+
 // Run npm install in the script's directory
 execSync('npm install', { cwd: dir, stdio: 'inherit' })
